29636ac04289df38d4640d10cee732f998852a3b
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-20 12:19:57 +0300

    Added links to buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
     <>
         <header class="d-flex justify-content-center py-3 text-bg-dark">
             <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link text-white active" aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white">About</a></li>
+                <Link to="/"><li class="nav-item"><a href="#" class="nav-link text-white active" aria-current="page">Home</a></li></Link>
+                <Link to="/about"><li class="nav-item"><a href="#" class="nav-link text-white">About</a></li></Link>
+                <Link to="/among"><li class="nav-item"><a href="#" class="nav-link text-white">Among</a></li></Link>
+                <Link to="/us"><li class="nav-item"><a href="#" class="nav-link text-white">Us</a></li></Link>
             </ul>
         </header>
     </>
