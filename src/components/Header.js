83d1b9ee2378012f1351a6d5f9f79d51449157c6
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-20 13:08:10 +0300

    Added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../logo.webp';
 
 export default function Header() {
     return (
     <>
-        <header class="d-flex justify-content-center py-3 text-bg-dark">
-            <ul class="nav nav-pills">
+        <header class="d-flex justify-content-left py-3 px-4 text-bg-dark">
+            <div class="logo"><img src={logo} alt="Page Logo" /></div>
+            <ul class="nav nav-pills mx-5">
                 <Link to="/"><li class="nav-item"><a href="#" class="nav-link text-white active" aria-current="page">Home</a></li></Link>
                 <Link to="/about"><li class="nav-item"><a href="#" class="nav-link text-white">About</a></li></Link>
                 <Link to="/among"><li class="nav-item"><a href="#" class="nav-link text-white">Among</a></li></Link>
