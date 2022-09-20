2aaedcc59513747725dd78a84dc6149f5d5735d9
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-20 12:19:24 +0300

    Created extra pages

---
+++ src/pages/Among.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Among() {
+  return (
+    <div>
+        <h1>Among</h1>
+        <img src="https://static.wikia.nocookie.net/jerma-lore/images/e/e3/JermaSus.jpg/revision/latest?cb=20201206225609" alt="JermaSus" />
+        <Link to="/"><button>Home</button></Link>
+    </div>
+  )
+}
--- src/pages/Us.js
+++ src/pages/Us.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function About() {
+export default function Us() {
   return (
     <div>
-        <h1>About</h1>
+        <h1>Us</h1>
         <Link to="/"><button>Home</button></Link>
     </div>
   )
