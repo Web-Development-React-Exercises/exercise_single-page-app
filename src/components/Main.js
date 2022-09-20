52403a4eadf7fa1372affd8d17ba25c93904f15e
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-20 13:34:57 +0300

    Created main component

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div id="main">
+        <div class='main-container'>
+        <h1>Welcome Among Us</h1>
+        <h2>Gaming Revolution</h2>
+        </div>
+    </div>
+  )
+}
