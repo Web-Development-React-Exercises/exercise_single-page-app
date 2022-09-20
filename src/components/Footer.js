6cb86f50e8c54cb6932c8c3e62f57cf23bd21301
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-20 13:34:47 +0300

    Darkened Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div>
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 mt-4 border-top">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 mt-4 text-bg-dark">
                 <div class="col-md-4 d-flex align-items-center">
                     <span class="mx-3 mb-md-0 text-muted">© 2022 Crewmate, Inc</span>
                 </div>
