5c605544fb9a23611512766dc459b2771b1d2258
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-20 12:26:17 +0300

    Removed meme image

--- src/pages/Among.js
+++ src/pages/Among.js
@@ -5,7 +5,6 @@ export default function Among() {
   return (
     <div>
         <h1>Among</h1>
-        <img src="https://static.wikia.nocookie.net/jerma-lore/images/e/e3/JermaSus.jpg/revision/latest?cb=20201206225609" alt="JermaSus" />
         <Link to="/"><button>Home</button></Link>
     </div>
   )
